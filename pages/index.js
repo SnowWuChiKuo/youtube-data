b20a487b62bffeeb6b9339c81e931d8c328a5e4c
SnowWuChiKuo <https://github.com/SnowWuChiKuo>
2024-07-29 21:33:07 +0800

    add home page data for Pagination

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import SearchApi from "../components/searchApi";
 export default function Home() {
   const [inputString, setInputString] = useState('');
   const { fetchedData, setFetchedData, selectedItems, setSelectedItems } = useContext(DataContext);
+  const [currentPage, setCurrentPage] = useState(5);
+  const itemsPerPage = 5;
 
   const handleClick = () => {
     refetch();
@@ -23,7 +25,7 @@ export default function Home() {
   const handleSearch = async ({ queryKey }) => {
     const [, inputWord] = queryKey;
     const auth = process.env.NEXT_PUBLIC_YOUTUBE_KEY;
-    const searchUrl = `https://www.googleapis.com/youtube/v3/search?&q=${inputWord}&key=${auth}&part=snippet&type=channel&order=relevance`;
+    const searchUrl = `https://www.googleapis.com/youtube/v3/search?&q=${inputWord}&key=${auth}&part=snippet&type=channel&order=relevance&maxResults=50`;
     const dataFetch = await fetch(searchUrl, {
         method: "GET",
         headers: {
@@ -36,8 +38,8 @@ export default function Home() {
           const jsonData = await dataFetch.json();
           const data = jsonData.items;
           const result = data.map((item) => ({
-            id: item.id,
-            channelId: item,
+            id: item.id.channelId,
+            channelId: item.snippet.channelId,
             title: item.snippet.title,
             description: item.snippet.description,
             thumbnails: item.snippet.thumbnails.default.url,
@@ -53,6 +55,8 @@ export default function Home() {
 
   const { error, isLoading, refetch } = useQuery({ queryKey:['getData', inputString], queryFn: handleSearch, enabled: false })
 
+  console.log('fD: ', fetchedData);
+
   const handleAddItem = (item) => {
     if (!selectedItems.some(selectedItem => selectedItem.id === item.id)) {
       setSelectedItems([...selectedItems, item]);
@@ -62,6 +66,27 @@ export default function Home() {
   const handleRemoveItem = (index) => {
     setSelectedItems(selectedItems.filter((_, i) => i !== index));
   }
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    } 
+  }
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(fetchedData.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  console.log('currentPage: ', currentPage);
+
+  // 單頁最後一個數量
+  const indexLastItem = currentPage * itemsPerPage;
+  // 單頁第一個數量
+  const indexFirstItem = indexLastItem - itemsPerPage;
+  // 當前頁面的數量
+  const currentItems = fetchedData?.slice(indexFirstItem, indexLastItem);
 
   return (
       <div className="col-span-4 grid grid-rows-10">
@@ -84,7 +109,24 @@ export default function Home() {
               </li>
             </ul>
           </div>
-          <SearchApi data={fetchedData} onAddItem={handleAddItem} />
+          <SearchApi data={currentItems} onAddItem={handleAddItem} />
+          {fetchedData && <div className="flex justify-center text-center ">
+            <button 
+              className="bg-amber-400 text-black rounded-full p-3 mr-5 hover:bg-blue-500"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              上一頁
+            </button>
+            <p className="bg-amber-400 text-black rounded-full p-3 mr-5">{currentPage}</p>
+            <button 
+              className="bg-amber-400 text-black rounded-full p-3 mr-5 hover:bg-blue-500"
+              onClick={handleNextPage}
+              disabled={currentPage === Math.ceil(fetchedData.length / itemsPerPage)}
+              >
+                下一頁
+            </button>
+          </div>}
         </main>
         <Release items={selectedItems} onRemoveItem={handleRemoveItem} />
       </div>
